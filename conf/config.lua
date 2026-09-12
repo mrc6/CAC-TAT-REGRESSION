da34de66814722f2af75e2a9938ddb1489d75444
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2026-09-12 16:45:31 -0300

    fix: Update browser webdriver executable path

--- conf/config.lua
+++ conf/config.lua
@@ -4,7 +4,7 @@
  --All paths are relative to the conf folder of the project
 local config = {
     browser_webdriver_path = "../drivers",
-    browser_webdriver_executable = "geckodriver --connect-existing",
+    browser_webdriver_executable = "geckodriver",
     test_cases_path = "../examples",
 }
 
